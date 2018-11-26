cb94a4ba4a0aaa69fd641be4bb5c61d4f009f513
pajeet063 <39622164+mhspes@users.noreply.github.com>
2018-11-26 21:53:35 +0200

    Add files via upload

---
+++ base_station.ino
@@ -0,0 +1,84 @@
+#include <XBee.h>
+
+XBee xbee = XBee();
+XBeeResponse response = XBeeResponse();
+Rx16Response rx16 = Rx16Response();
+
+uint8_t sensor_data[8];
+uint16_t addr16;
+uint16_t addr_list[] = {4097,4098,4099}; // List of allowed addresses
+
+void setup() {
+  Serial.begin(9600);
+  xbee.setSerial(Serial);
+}
+
+void loop() { 
+  xbee.readPacket();
+  
+  if (xbee.getResponse().isAvailable()){
+    
+    if (xbee.getResponse().getApiId() == RX_16_RESPONSE){
+      
+     xbee.getResponse().getRx16Response(rx16);
+     addr16 = rx16.getRemoteAddress16();
+
+      if(addr16 == addr_list[0] || addr16 == addr_list[1] || addr16 == addr_list[2]){
+        Serial.print("Packet received from ");
+        switch(addr16){
+        case 4097:
+          Serial.print(addr16);
+          Serial.println(" - Node 1.");
+          break;
+        case 4098:
+          Serial.print(addr16);
+          Serial.println(" - Node 2.");
+          break;
+        case 4099:
+          Serial.print(addr16);
+          Serial.println(" - Node 3.");
+          break;
+        default:
+          Serial.println("unknown address.");
+          break;
+         }
+     
+        //Fetch the temperature data
+      for (int i =rx16.getDataLength()-1; i >= 0;i--){
+        sensor_data[i] = rx16.getData(i);
+        }
+      print_data(sensor_data);
+        //Print measurements 
+      } else {
+        Serial.println("Unknown address - ignoring packet."); 
+      }
+    } else {
+      Serial.println("Wrong addressing format - 16bit expected.");
+    }
+    delay(1000);
+    
+    } else if (xbee.getResponse().isError()) {
+      Serial.println("Error reading packet.");     
+    } 
+}
+
+// Routine for printing data from
+// temperature & humidity sensor
+void print_sensordata(uint8_t data){
+
+  Serial.print("Temperature: ");
+  for (int i = sizeof(data)-1; i >= 0; i-- ){
+    Serial.print(data[i]); 
+    if (i==4){
+      Serial.print(" C Humidity: ");
+      }
+    if (i==0){
+      Serial.print(" %");
+      }
+    if (i==6 || i==2){
+      Serial.print(".");
+      }
+    }
+      Serial.println();
+}
+
---
+++ temp_node.ino
@@ -0,0 +1,54 @@
+#include <XBee.h>
+#include <DHT.h>
+
+#define DHTTYPE DHT22 // Temp&hum sensor
+#define dataPin 10
+#define powerPin 8    // Pin for powering the sensor
+
+XBee xbee = XBee();
+DHT dht(dataPin,DHT22);
+uint8_t data[8];
+
+// Packet for transmitting (address 0x1000 - base station)
+uint16_t addr16 = 0x1000;
+Tx16Request tx = Tx16Request(addr16,data,sizeof(data));
+
+//Setup
+void setup() {
+  Serial.begin(9600);
+  xbee.setSerial(Serial);
+  dht.begin();
+  pinMode(powerPin,OUTPUT);
+}
+
+
+void loop() {
+  // Temperature & humidity -sensor is activated
+  // Sensor is powered only during the measuring
+  digitalWrite(powerPin,HIGH);
+  delay(1000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  digitalWrite(powerPin,LOW);
+  delay(1000);
+  
+  //Convert digits into one uint8_t array
+  // 36.80 %, 26.50 *C --> 3680, 2650 (int) 
+  int temp1 = ((double)t)*100;
+  int temp2 = ((double)h)*100;
+  for(int i = 3; i >=0; i--){
+    data[i+4]= ((temp1 / (int)pow(10,i)) % 10);
+    data[i]= ((temp2 /(int)pow(10,i)) % 10);
+  }
+
+  //Print address and transmitted data
+  Serial.print("Sending data packet to: ");
+  Serial.println(addr16);
+  for(int i = 7; i>=0 ; i--){
+    Serial.print(data[i]); // printf("d",data[i]);
+  }
+  Serial.print("\n");
+  xbee.send(tx);
+  delay(1000);
+}
+
